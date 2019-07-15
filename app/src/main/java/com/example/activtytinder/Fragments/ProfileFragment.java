59b22d72a3f42f4937f7d929b1159eaf82dce002
Annalice Ni <anni00@fb.com>
2019-07-15 16:43:40 -0700

    added fragments

---
+++ app/src/main/java/com/example/activtytinder/Fragments/ProfileFragment.java
@@ -0,0 +1,6 @@
+package com.example.activtytinder.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class ProfileFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/activtytinder/Fragments/ReceiptFragment.java
@@ -0,0 +1,6 @@
+package com.example.activtytinder.Fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class ReceiptFragment extends Fragment {
+}
