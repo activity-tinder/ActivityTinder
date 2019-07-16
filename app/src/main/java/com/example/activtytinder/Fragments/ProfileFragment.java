2acad58a9614c2e5b5cba986c5a99081495fdee5
Brad Chavero-Correa <bradc117@fb.com>
2019-07-16 13:03:41 -0700

    Added Login Button added in Profile Fragment.

--- app/src/main/java/com/example/activtytinder/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/activtytinder/Fragments/ProfileFragment.java
@@ -1,6 +1,52 @@
 package com.example.activtytinder.Fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.activtytinder.MainActivity;
+import com.example.activtytinder.R;
+import com.parse.ParseUser;
+
 public class ProfileFragment extends Fragment {
+
+    public Button logoutButton;
+    public static final String TAG = "ProfileFragment";
+
+    //onCreateView to inflate the view
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false); //returns appropriate view
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        logoutButton = view.findViewById(R.id.logout_btn);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+    }
+
+    private void logout(){
+        ParseUser.logOut();
+        ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+        final Intent intent = new Intent(getActivity(), MainActivity.class);
+        //start the intent that switches to home activity to in turn go to the home activity
+        startActivity(intent);
+        getActivity().finish();
+    }
+
 }
