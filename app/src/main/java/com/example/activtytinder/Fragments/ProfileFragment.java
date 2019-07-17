4c95f7311e4443d91d78d250d3c84b8681c16565
Brad Chavero-Correa <bradc117@fb.com>
2019-07-17 01:02:21 -0700

    Finished Location and Profile Page Initial.

--- app/src/main/java/com/example/activtytinder/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/activtytinder/Fragments/ProfileFragment.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
@@ -12,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,7 +20,6 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.activtytinder.LoginActivity;
-import com.example.activtytinder.MainActivity;
 import com.example.activtytinder.R;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -28,13 +27,9 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.model.LatLng;
 import com.parse.ParseUser;
 
-import java.security.Permissions;
-
 import permissions.dispatcher.NeedsPermission;
-import permissions.dispatcher.PermissionUtils;
 
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
@@ -42,13 +37,14 @@ public class ProfileFragment extends Fragment {
 
     public Button logoutButton;
     public Button getLocationButton;
+    public TextView tvName;
+    public TextView tvUsername;
+    public TextView tvEmail;
+    public TextView tvScore;
+    public TextView tvAge;
+    public TextView tvBaseLocation;
     public static final String TAG = "ProfileFragment";
     private LocationRequest mLocationRequest;
-    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
-    private long FASTEST_INTERVAL = 2000; /* 2 sec */
-
-
-
 
     //onCreateView to inflate the view
     @Nullable
@@ -61,7 +57,12 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         logoutButton = view.findViewById(R.id.logout_btn);
         getLocationButton = view.findViewById(R.id.get_location_btn);
-    //    startLocationUpdates();
+        tvName = view.findViewById(R.id.tvName);
+        tvUsername = view.findViewById(R.id.tvUsername);
+        tvEmail = view.findViewById(R.id.tvEmail);
+        tvScore = view.findViewById(R.id.tvScore);
+        tvAge = view.findViewById(R.id.tvAge);
+        tvBaseLocation = view.findViewById(R.id.tvBaseLocation);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,17 +70,16 @@ public class ProfileFragment extends Fragment {
                 logout();
             }
         });
-
         getLocationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startLocationUpdates();
+                getCurrentLocation();
             }
         });
 
     }
 
-    private void logout(){
+    private void logout() {
         ParseUser.logOut();
         ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
         final Intent intent = new Intent(getActivity(), LoginActivity.class);
@@ -88,15 +88,12 @@ public class ProfileFragment extends Fragment {
         getActivity().finish();
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint({"MissingPermission", "NewApi"})
     @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
-    // Trigger new location updates at interval
-    protected void startLocationUpdates() {
+    private void getCurrentLocation() {
         // Create the location request to start receiving updates
         mLocationRequest = new LocationRequest();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
         builder.addLocationRequest(mLocationRequest);
@@ -105,57 +102,26 @@ public class ProfileFragment extends Fragment {
         SettingsClient settingsClient = LocationServices.getSettingsClient(getActivity());
         settingsClient.checkLocationSettings(locationSettingsRequest);
 
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-            
+        if (getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION}, 20);
+        }else {
+            Log.e(TAG, "PERMISSION GRANTED");
         }
-
-
-//        if (ActivityCompat.checkSelfPermission(getContext(),
-//                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-//                ActivityCompat.checkSelfPermission(getContext(),
-//                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            requestPermissions(getActivity(),
-//                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION,
-//                            android.Manifest.permission.ACCESS_FINE_LOCATION},
-//                    REQUEST_LOCATION);
-//        } else {
-//            Log.e("DB", "PERMISSION GRANTED");
-//        }
-//        getFusedLocationProviderClient(getActivity()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
-//                    @Override
-//                    public void onLocationResult(LocationResult locationResult) {
-//                        onLocationChanged(locationResult.getLastLocation());
-//                    }
-//                },
-//                Looper.myLooper());
-//    }
-
-
-    public void onLocationChanged(Location location) {
-        // New location has now been determined
-        String msg = "Updated Location: " +
-                Double.toString(location.getLatitude()) + "," +
-                Double.toString(location.getLongitude());
-        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
-        // You can now create a LatLng Object for use with maps
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        Log.e(TAG, String.valueOf(locationSettingsRequest));
+        getFusedLocationProviderClient(getActivity()).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        //onLocationChanged(locationResult.getLastLocation());
+                        String msg = "Location: " +
+                                Double.toString(locationResult.getLastLocation().getLatitude()) + "," +
+                                Double.toString(locationResult.getLastLocation().getLongitude());
+                        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                Looper.myLooper());
     }
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        getActivity().onRequestPermissionsResult(ProfileFragment.class, requestCode, grantResults);
-//
-//    }
-
-
-
 }
+
+
