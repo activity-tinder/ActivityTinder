80ef3e9ebf378849df24d2b72a269abc3ed4e874
Annalice Ni <annaliceni@gmail.com>
2019-07-19 16:24:04 -0700

    fixed card swiping

--- app/src/main/java/com/example/activtytinder/Fragments/CardFragment.java
+++ app/src/main/java/com/example/activtytinder/Fragments/CardFragment.java
@@ -23,8 +23,6 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static com.example.activtytinder.Fragments.ProfileFragment.TAG;
@@ -36,15 +34,13 @@ public class CardFragment extends Fragment {
     private ImageButton btnReject;
 
     SwipePlaceHolderView mSwipePlaceHolderView;
-    List<Event> events;
-
+    Point cardViewHolderSize;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_card, container, false);
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -53,15 +49,15 @@ public class CardFragment extends Fragment {
         btnAccept = view.findViewById(R.id.acceptBtn);
         btnReject = view.findViewById(R.id.rejectBtn);
 
-        events = new ArrayList<>();
+        int bottomMargin = Utils.dpToPx(160);
+        Point windowSize = Utils.getDisplaySize(getActivity().getWindowManager());
+        cardViewHolderSize = new Point(windowSize.x, windowSize.y - bottomMargin);
 
         mSwipePlaceHolderView = view.findViewById(R.id.swipeView);
 
-        int bottomMargin = Utils.dpToPx(160);
-        Point windowSize = Utils.getDisplaySize(getActivity().getWindowManager());
-        Point cardViewHolderSize = new Point(windowSize.x, windowSize.y - bottomMargin);
 
-        queryPosts(cardViewHolderSize);
+
+        queryEvents();
 
         mSwipePlaceHolderView.getBuilder()
                 .setDisplayViewCount(3)
@@ -70,14 +66,6 @@ public class CardFragment extends Fragment {
                 .setSwipeDecor(new SwipeDecor()
                     .setPaddingTop(20)
                     .setRelativeScale(0.01f));
-
-//        Point cardViewHolderSize = new Point(windowSize.x, windowSize.y - bottomMargin);
-
-//        System.out.println(events.toString() + "oof");
-//
-//        for (Event event : events) {
-//            mSwipePlaceHolderView.addView(new SwipeEventCard(getContext(), event, cardViewHolderSize, (SwipeEventCard.Callback) this));
-//        }
 
         //TODO -- figure out what v is
         btnAccept.setOnClickListener(v -> {
@@ -92,28 +80,27 @@ public class CardFragment extends Fragment {
 
     }
 
-    private void queryPosts(Point cardViewHolderSize) {
+    /*
+    Gets the events from the database and puts them into the SwipePlaceHolderView card stack.
+
+     */
+    private void queryEvents() {
         ParseQuery<Event> eventQuery = new ParseQuery<Event>(Event.class);
-        Toast.makeText(getContext(), "got into queryPosts", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), "got into queryEvents", Toast.LENGTH_SHORT).show();
         eventQuery.include(Event.KEY_CREATOR);
         eventQuery.setLimit(5);
         eventQuery.findInBackground(new FindCallback<Event>() {
             @Override
             public void done(List<Event> objects, ParseException kms) {
-                Toast.makeText(getContext(), "got into done", Toast.LENGTH_SHORT).show();
+
                 if (kms != null) {
-                    Toast.makeText(getContext(), "got into check", Toast.LENGTH_SHORT).show();
                     Log.d(TAG, "Error with Parse Query");
                     kms.printStackTrace();
                     return;
                 }
-                Toast.makeText(getContext(), "got over done", Toast.LENGTH_SHORT).show();
-                events.addAll(objects);
-                Log.d(TAG, "Posts: " + Arrays.toString(events.toArray()));
 
                 for (int i = 0; i < objects.size(); i++) {
-//                Event event = objects.get(i);
-//                events.add(event)
+
                   mSwipePlaceHolderView.addView(new SwipeEventCard(CardFragment.this.getContext(), objects.get(i), cardViewHolderSize));
 
                   Log.d(TAG, "Post: "
