d05d781e32f9d265bdd46b5a33b5b109efbaeb8d
Annalice Ni <annaliceni@gmail.com>
2019-07-23 17:53:05 -0700

    edited navbar

--- app/src/main/java/com/example/activtytinder/MainActivity.java
+++ app/src/main/java/com/example/activtytinder/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView.set
         final FragmentManager fragmentManager = getSupportFragmentManager();
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
