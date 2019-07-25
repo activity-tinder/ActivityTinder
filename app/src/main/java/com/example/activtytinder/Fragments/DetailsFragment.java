92d14b82b966899047f137b8cde4e40c5a9e1f3b
Omotara Oloye <omotara21@gmail.com>
2019-07-24 22:55:07 -0700

    Finished basic detailDialogFragment

--- app/src/main/java/com/example/activtytinder/Fragments/DetailsFragment.java
+++ app/src/main/java/com/example/activtytinder/Fragments/DetailsFragment.java
@@ -5,16 +5,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.activtytinder.Models.Event;
+import com.example.activtytinder.R;
 
+import org.json.JSONArray;
 import org.parceler.Parcels;
 
 public class DetailsFragment extends DialogFragment {
+
+    private TextView tvEventDetailsFragment;
+    private Button btnClose;
+    private Event mEvent;
+    private String mName;
+    private String mDate;
+    private String mLocation;
+    private String mCreator;
+    private JSONArray mAttendees;
+    private Integer mLimit;
+    private String mDescription;
+    private String mStartTime;
+    private String mEndTime;
 
     public DetailsFragment() {
 
@@ -29,26 +46,67 @@ public class DetailsFragment extends DialogFragment {
         return fragment;
     }
 
-    Event mEvent;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Bundle eventBundle = this.getArguments();
-        if(eventBundle != null){
-            mEvent = Parcels.unwrap(eventBundle.getParcelable("Event"));
-        }
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.fragment_details, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        tvEventDetailsFragment = view.findViewById(R.id.tvEventDetailsFragment);
+        btnClose = view.findViewById(R.id.btnClose);
 
+        Bundle eventBundle = this.getArguments();
+        if(eventBundle != null){
+            mEvent = Parcels.unwrap(eventBundle.getParcelable("Event"));
+        }
 
         String eventDetails = getArguments().getString("Event Details", "Event Details");
         getDialog().setTitle(eventDetails);
 
+        tvEventDetailsFragment.requestFocus();
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+
+        mName = mEvent.getKeyName();
+        mDate = mEvent.getKeyDate();
+        mLocation = mEvent.getKeyAddress();
+        mCreator = mEvent.getCreator().getUsername();
+        mAttendees = mEvent.getKeyAttendees();
+        mLimit = mEvent.getKeyLimit();
+        mDescription = mEvent.getKeyDescription();
+        mStartTime = mEvent.getKeyStartTime();
+        mEndTime = mEvent.getKeyEndTime();
+
+        tvEventDetailsFragment.setText(
+                "Name: "
+                        + mName
+                        + "\n\nDate: "
+                        + mDate
+                        + "\n\nTime: "
+                        + mStartTime
+                        + " - "
+                        + mEndTime
+                        + "\n\nCreated by: "
+                        + mCreator
+                        +"\n\nLocation: "
+                        + mLocation
+                        + "\n\nAttendees: "
+                        + mAttendees
+                        + "\n\nPeople Limit: "
+                        + mLimit
+                        + "\n\nDescription: "
+                        + mDescription
+                        + "\n\n"
+        );
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
     }
 }
