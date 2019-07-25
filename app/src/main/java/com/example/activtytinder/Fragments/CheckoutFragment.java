ef60a39cd4678e7cba6da381aeecbf16dabd06f3
Annalice Ni <annaliceni@gmail.com>
2019-07-25 13:15:30 -0700

    Update CheckoutFragment.java

--- app/src/main/java/com/example/activtytinder/Fragments/CheckoutFragment.java
+++ app/src/main/java/com/example/activtytinder/Fragments/CheckoutFragment.java
@@ -126,7 +126,7 @@ public class CheckoutFragment extends DialogFragment {
             @Override
             public void onClick(View view) {
                 //TODO -- close overlay and take them back to the cards
-                btnNoListener.onNoClicked();
+                //btnNoListener.onNoClicked();
                 dismiss();
             }
         });
