8c193b6528d6191b858f9d50e8c4ebda7b159c69
Brad Chavero-Correa <bradc117@fb.com>
2019-08-13 14:00:25 -0700

    GeoLocation Attempt. Crashes after following documentation.

--- app/src/main/java/com/example/activtytinder/GeoFenceBroadcastReceiver.java
+++ app/src/main/java/com/example/activtytinder/GeoFenceBroadcastReceiver.java
@@ -3,10 +3,47 @@ package com.example.activtytinder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
 
 public class GeoFenceBroadcastReceiver extends BroadcastReceiver {
+
+    private final String TAG = "GeoFence Receiver";
     @Override
     public void onReceive(Context context, Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+//            String errorMessage = GeofenceErrorMessages.getErrorString(this,
+//                    geofencingEvent.getErrorCode());
+            Log.e(TAG, Integer.toString(geofencingEvent.getErrorCode()));
+            return;
+        }
 
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
+                geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+            // Get the transition details as a String.
+//            String geofenceTransitionDetails = getGeofenceTransitionDetails(this, geofenceTransition, triggeringGeofences);
+
+            // Send notification and log the transition details.
+//            sendNotification(geofenceTransitionDetails);
+//            Log.i(TAG, geofenceTransitionDetails);
+        } else {
+            // Log the error.
+//            Log.e(TAG, getString(R.string.geofence_transition_invalid_type, geofenceTransition));
+            Log.e(TAG, Integer.toString(geofencingEvent.getErrorCode()));
+        }
     }
 }
